180855a29e1afd021818ba61396e5ca1a203d645
Steven Tu <stu562@gmail.com>
2019-01-28 11:58:14 -0800

    premapping what should be implemented

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,25 @@
+console.log('hey app is working')
+//onclick place x  or an O 
+element.attachEvent('onclick', 'i made it happen');
+
+// var game = {
+//     user: '',
+//     computer: '',
+//     currentPlayer: '',
+//     moves: 1,
+//   };
 
 
+//create a matrix 
+var board = [['','',''],
+            ['','',''],
+            ['','','']]
+//when place a piece , incremtn moves, max move is 9 
+//Player 1 or Player 2 
+//when cliked event
+    //place an X in square
+    // switch to player 2 
+    //place an O in the selected square 
+//detect a win or tie 
+    //need to look through matrix 
+    // check if win or not 
