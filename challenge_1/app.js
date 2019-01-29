4f63784e85887aa9bf58ffb319b9472512ac34ac
Steven Tu <stu562@gmail.com>
2019-01-29 12:11:20 -0800

    more notes

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,6 +1,7 @@
 console.log('hey app is working')
 //onclick place x  or an O 
-element.attachEvent('onclick', 'i made it happen');
+// element.attachEvent('onclick', 'i made it happen');
+// document.getElementById('r1').onclick = forgotpass;
 
 // var game = {
 //     user: '',
@@ -9,12 +10,11 @@ element.attachEvent('onclick', 'i made it happen');
 //     moves: 1,
 //   };
 
+const cells = document.querySelectorAll('.cells')
 
-//create a matrix 
-var board = [['','',''],
-            ['','',''],
-            ['','','']]
-//when place a piece , incremtn moves, max move is 9 
+//when place a piece , increment moves, max move is 9 
+
+
 //Player 1 or Player 2 
 //when cliked event
     //place an X in square
@@ -22,4 +22,32 @@ var board = [['','',''],
     //place an O in the selected square 
 //detect a win or tie 
     //need to look through matrix 
-    // check if win or not 
+    // if all strings, in the row is the same then alert(win)!
+    //rows, verticals and diagonal
+
+    //create a matrix 
+var board = 
+            [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+            ];
+var possibleWins =   [ 
+                        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+                        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                        [0, 4, 8], [6, 4, 2]
+                    ];
+
+
+//check the board after every turn
+// if array matches any of the possibleWins the n
+// return the winner 
+var checkForWinner = function(board){
+    //outer
+    for (var i = 0; i < board.length; i++){
+        for (var j = i; j < board.length; j++){
+
+
+        }
+    }
+}
