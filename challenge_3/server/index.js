bbd745a8a071558e2eef960e1fdc45850268d525
Steven Tu <stu562@gmail.com>
2019-01-30 19:27:44 -0800

    fix get and post

--- challenge_3/server/index.js
+++ challenge_3/server/index.js
@@ -7,13 +7,20 @@ const app = express();
 app.use(paser.json());
 app.use(parser.urlencoded({extended:true}));
 app.use('/shopping', routes);//different endpoints ? 
-
+app.use(express.static(path.join(__dirname, '../public')));//location of stactic files
 // base route
 app.get("/shopping", function (req, res) {
-    response.send("Get Stuff") 
-});
+    getUser() // show all info, and render on the last page 
+    .then((data) => res.status(200).send(data))
+    .catch((err) => res.status(404).send(err))
+})
+
 app.post("/shopping", function (req, res) {
-    response.send("Post Stuff") 
-});
+    //this needs to update the database info
+    createUser()
+    //need to add something here 
+    .then((data) => res.status(201).send(data))
+    .catch((err) => res.status(404).send(err));
+})
 
 app.listen(port, Console.log('hey from server: ', port));
