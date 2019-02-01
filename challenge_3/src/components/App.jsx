30a46b65b56b1692a5038b10cfb00ceb9ca89fac
Steven Tu <stu562@gmail.com>
2019-01-31 16:37:05 -0800

    work in progress for multi page

--- challenge_3/src/components/App.jsx
+++ challenge_3/src/components/App.jsx
@@ -14,44 +14,29 @@ class App extends React.Component {
       line1:'',
       line2:'',
       city:'',
-      zip:''
+      zip:'',
+      creditNumber:'',
+      expiry:'',
+      cvv:'',
+      billingZip:''
     };
-    // this.handleInput = this.handleInput.bind(this); 
-    // this.handleChange = this.handleChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
     // this.handleNext = this.handleNext.bind(this); 
     // this.formOne = this.formOne.bind(this); 
     // this.formTwo = this.formTwo.bind(this); 
     // this.formThree = this.formThree.bind(this); 
     
-
-
   }
-
-
 
 
   // componentDidMount() {
   //   this.getAllItems();
   // }
   
-  // onChange (event) {
-  //   console.log(event.target.name); // the name of the form element
-  //   console.log(event.target.value); // the value of the form element
-  // }
-
-
-
-  handleInput(e) {
-    //the user will put in input value 
-    let { value } = e.target;
-    console.log(e.target)
-    // this.setState({}, () => console.log(this.state.))
-    // this.setState({ })
-  }
 
   handleChange (e) {
-    //bracket syntax
     this.setState({ [e.target.name]: e.target.value });
+    // () => console.log(this.state.name)
   }
 
   // handleNext(){
@@ -68,51 +53,43 @@ class App extends React.Component {
   //     )
   // }
 
-  //F2collects ship to address (line 1, line 2, city, state, zip code) and phone number.  
-  // formTwo (){
-  //   return (
-  //     <div> Testing from formOne
-  //       <input type="text"/>
-  //       <button type="submit">Next</button>
-  //       </div>
-  //   )
-  // }
-
+//F2collects ship to address (line 1, line 2, city, state, zip code) and phone number.  
 // collects credit card #, expiry date, CVV, and billing zip code
-  // formThree (){
-  //   return (
-  //     <div> Testing from formOne
-  //       <input type="text"/>
-  //       <button type="submit">Next</button>
-  //       </div>
-  //   )
-  // }
-
 
   render() {
     return (
-      <div> testing from App react
+      <div> <h1>React Wants your info, yo!</h1>
         <form>
+          <h3>{this.state.name}</h3>
+          {/* just to test if  working */}
           <label> Name </label>
-          <input type="text" name={this.state.name} onChange={this.handleChange}/><br />
+          <input type="text" name="name" onChange={this.handleChange}/><br />
           <label> Email </label>
-          <input type="text" name={this.state.email} onChange={this.handleChange} /><br />
+          <input type="text" name="email" onChange={this.handleChange} /><br />
           <label> Password </label>
-          <input type="text" name={this.state.password} onChange={this.handleChange} /><br />
+          <input type="text" name="password" onChange={this.handleChange} /><br />
+          <button type="button" onClick={() => console.log('next page please')}>Next</button><br />
+
+
           <label> Line1 </label>
-          <input type="text" name={this.state.line1} onChange={this.handleChange} /><br />
+          <input type="text" name="line1" onChange={this.handleChange} /><br />
           <label> Line2 </label>
-          <input type="text" name={this.state.line2} onChange={this.handleChange} /><br />
+          <input type="text" name="line2" onChange={this.handleChange} /><br />
           <label> City </label>
-          <input type="text" name={this.state.city} onChange={this.handleChange} /><br />
+          <input type="text" name="city" onChange={this.handleChange} /><br />
           <label> Zip </label>
-          <input type="text" name={this.state.zip} onChange={this.handleChange} /><br />
+          <input type="text" name="zip" onChange={this.handleChange} /><br />
         </form>
+        <button type="button" onClick={() => console.log('next page please')}>Next</button><br />
 
 
       </div>
     )
   } 
+
+  render2(){
+    return ()
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
